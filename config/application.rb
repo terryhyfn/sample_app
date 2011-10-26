3d41a421d56165d18cd14c0c1fd7e7c6cd155ce5
Terry <terry@hyfn.com>
2011-10-26 16:58:08 -0700

    Done with static pages

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,12 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    ### Part of a Spork hack.
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
   end
 end
